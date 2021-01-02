9e19e1bc440eef6b41fb15e6472d69c5f7572406
Ricky <rickdhanota@gmail.com>
2021-01-01 23:04:48 -0800

    made adjustments and opitmized

--- collectableCardGame_oop.js
+++ collectableCardGame_oop.js
@@ -4,8 +4,8 @@ class Card{
         this.cost = cost;
         return this;
     }
-    displayCardDetails(){
-        console.log(`Card Name: ${this.name}, and this Card's Cost is ${this.cost}`);
+    summonCard(){
+        console.log(`Summoning Card Name: ${this.name}, and this Card's Cost is ${this.cost}`);
         return this;
     }
 }
@@ -30,37 +30,22 @@ class Unit extends Card{
 }
 
 class Effects extends Card{
-    constructor(name, cost, text, stat, magnitude){
+    constructor(name, cost, text, stat, power, resilience){
         super(name, cost);
         this.text = text;
         this.stat = stat;
-        this.magnitude = magnitude;
+        this.power = power;
+        this.resilience = resilience;
         return this;
     }
-    resilienceEffect(target){
+    cardEffect(target){
         if(target instanceof Unit){
-            if(power){
+            if(this.stat == "Power"){
                 target.power += this.power;
                 return this;
             }
-            else if(this.magnitude){
+            else if(this.stat == "Resilience"){
                 target.resilience += this.magnitude;
-                return this;
-            }
-        }
-        else{
-            throw new Error( "Target must be a unit!" );
-        }
-    }
-
-    powerEffect(target){
-        if(target instanceof Unit){
-            if(power){
-                target.power += this.power;
-                return this;
-            }
-            else if(this.magnitude){
-                target.power += this.magnitude;
                 return this;
             }
         }
@@ -72,7 +57,9 @@ class Effects extends Card{
 
 const redBeltNinja = new Unit("Red Belt Ninja", 3, 3, 4);
 const blackBeltNinja = new Unit("Black Belt Ninja", 4, 5, 4);
-const hardAlgorithm = new Effects("Hard Algorithm", 2, "increase target's resilience by 3", "resilience", 3);
-const unhandledPromiseRejection = new Effects("Unhandled Promise Rejection", 1, "reduce target's resilience by 2", "resilience", -2);
-const pairProgramming = new Effects("Pair Programming", 3, "increase target's power by 2", "Power", 2);
+const hardAlgorithm = new Effects("Hard Algorithm", 2, "increase target's resilience by 3", "Resilience", 0, 3);
+const unhandledPromiseRejection = new Effects("Unhandled Promise Rejection", 1, "reduce target's resilience by 2", "Resilience", 0, -2);
+const pairProgramming = new Effects("Pair Programming", 3, "increase target's power by 2", "Power", 2, 0);
 
+redBeltNinja.summonCard()
+blackBeltNinja.summonCard()
